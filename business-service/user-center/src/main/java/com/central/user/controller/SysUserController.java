0f07c01cf5b9f811acea5526f2f029fe284f17dd
253507605 <13295129572@163.com>
2023-05-25 19:52:26 +0800

    单元测试

--- business-service/user-center/src/main/java/com/central/user/controller/SysUserController.java
+++ business-service/user-center/src/main/java/com/central/user/controller/SysUserController.java
@@ -1,14 +1,34 @@
 package com.central.user.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author Jason.Chen
  * @create 2023/4/26 10:40
  */
 
-@RestController
+@Controller
+@RequestMapping("/sys")
 public class SysUserController {
 
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+
+    @RequestMapping("/toLogin")
+    public String toLogin(){
+        return "login";
+    }
+
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
+    private String login(String username,String password){
+        System.out.println(username);
+        System.out.println(password);
+        return "login";
+    }
 }
